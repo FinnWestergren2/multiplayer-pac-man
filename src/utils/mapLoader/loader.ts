7c2fa228285eed362fb0241888a8fca1ff79822a
FinnWestergren <fwesterg@skidmore.edu>
2020-05-23 18:05:24 -0400

    I cant figure this out

--- src/utils/mapLoader/loader.ts
+++ src/utils/mapLoader/loader.ts
@@ -2,11 +2,14 @@ import CellTypes from "./cellTypes";
 
 export const load: (path: string) => string = (path) => {
     var xhttp = new XMLHttpRequest();
-    xhttp.responseType = "document"
+    xhttp.responseType = "text"
     xhttp.onreadystatechange = () => {
         console.log(xhttp.response);
     };
-    xhttp.open("GET", "", true);
+    xhttp.open("GET", "http://localhost:8080/mapFile.csv", true);
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "localhost:8080");
+    xhttp.setRequestHeader("Access-Control-Request-Method", "GET");
+    xhttp.setRequestHeader("Origin", "http://localhost:3000");
     xhttp.send();
     return xhttp.response;
 };
