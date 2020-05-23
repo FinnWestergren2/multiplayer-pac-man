aeb7136a2cbdfec1df140ba5f6d04de435bb61f4
FinnWestergren <fwesterg@skidmore.edu>
2020-05-23 10:59:40 -0400

    dickin around

--- src/sketch/index.ts
+++ src/sketch/index.ts
@@ -1,30 +1,31 @@
 import * as p5 from 'p5';
+import { Player } from './Player';
 
 export default function sketch(p: p5) {
-    console.log(p);
-    let rotationY = 0;
-    let rotationX = 0;
+    const players: Player[] = []
+
 
     p.setup = function () {
-        p.createCanvas(600, 400, p.WEBGL);
+        p.createCanvas(400, 400);
+        var cols = 20;
+        var size = Math.min(p.width/(cols * Math.sqrt(2)), p.height/(cols * Math.sqrt(2)));
+        for(var i = 1; i < cols; i++){
+            for(var j = 1; j < cols; j++){
+                console.log(i, j);
+                const x = j * p.width/(cols);
+                const y = i * p.height/(cols);
+                players.push(new Player(x,y,size));
+            }
+        }
     };
 
     p.draw = function () {
-        p.background(100);
-        p.normalMaterial();
-        p.noStroke();
-        p.push();
-        p.rotateY(rotationY);
-        p.rotateX(rotationX);
-        p.box(100);
-        p.pop();
+        p.background(0);
+        players.forEach(pl => pl.draw(p));
     };
+    
 
     p.mouseWheel = (e: { delta: number}) => {
-        rotationY += e.delta * Math.PI / 180;
-    }
-
-    p.mousePressed = () => {
-        rotationX += 5 * Math.PI / 180;
+        players.forEach(pl => pl.rotate(e.delta * Math.PI / 180));
     }
 };
