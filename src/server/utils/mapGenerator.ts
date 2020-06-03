d3747402ac45741d17665390814f6e4f1019bb97
FinnWestergren <fwesterg@skidmore.edu>
2020-06-02 22:57:03 -0400

    Update mapGenerator.ts

--- src/server/utils/mapGenerator.ts
+++ src/server/utils/mapGenerator.ts
@@ -1,11 +1,31 @@
 import Stack from "./Stack"
 import Directions from "../../sketch/GameMap/directions"
+import CoordPair, { zeroPair } from "../../sketch/GameMap/CoordPair"
 
 type CellWithFlag = {
     dir: Directions;
     visited: boolean;
 }
 
+const emptyCell: CellWithFlag = {dir: Directions.NONE, visited: false}
+
+
 const generateMapUsingRandomDFS = () => {
+    const startingLocation: CoordPair = {...zeroPair};
+    const dimensions: CoordPair = {x: 10, y: 10}
+    const emptyMap = fillEmptyMap(dimensions);
     const stack: Stack<CellWithFlag> = new Stack<CellWithFlag>();
+}
+
+const fillEmptyMap = (dimensions: CoordPair) => {
+    const map: CellWithFlag[][] = [];
+    for (let i = 0; i < dimensions.x; i++) {
+        map.push(Array.from(emptyRow(dimensions.y)));
+    }
+}
+
+function* emptyRow(length: number){
+    for (let j = 0; j < length; j++) {
+        yield {...emptyCell}
+    }
 }
