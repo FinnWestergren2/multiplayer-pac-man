3ab58eb592ab6fa3391224a485bba8e906f1d433
Finn Westergren <fwesterg5@gmail.com>
2020-06-29 11:27:49 -0400

    Update gameState.ts

--- src/ducks/gameState.ts
+++ src/ducks/gameState.ts
@@ -4,25 +4,29 @@ import { PlayerCoordMap, PlayerDirectionMap } from "./sharedTypes";
 
 enum ActionTypes {
     RECORD_CURRENT_PLAYER_LOCATIONS = "RECORD_CURRENT_PLAYER_LOCATIONS",
-    RECORD_CURRENT_PLAYER_DIRECTIONS = "RECORD_CURRENT_PLAYER_DIRECTIONS"
+    RECORD_CURRENT_PLAYER_DIRECTIONS = "RECORD_CURRENT_PLAYER_DIRECTIONS",
+    RECORD_CURRENT_PLAYER_QUEUED_DIRECTIONS = "RECORD_CURRENT_PLAYER_QUEUED_DIRECTIONS",
 }
 
 type Action =
     { type: ActionTypes.RECORD_CURRENT_PLAYER_LOCATIONS; payload: { playerLocations: PlayerCoordMap, frame: number } } |
-    { type: ActionTypes.RECORD_CURRENT_PLAYER_DIRECTIONS; payload: { playerDirections: PlayerDirectionMap, frame: number } }
+    { type: ActionTypes.RECORD_CURRENT_PLAYER_DIRECTIONS; payload: { playerDirections: PlayerDirectionMap, frame: number } } | 
+    { type: ActionTypes.RECORD_CURRENT_PLAYER_QUEUED_DIRECTIONS; payload: { playerDirections: PlayerDirectionMap, frame: number } } 
+
+type framedState<T> = {
+    [frame: number]: T
+} 
 
 type gameState = {
-    playerLocations: {
-        [frame: number]: PlayerDirectionMap;
-    }
-    playerDirections: {
-        [frame: number]: PlayerDirectionMap;
-    }
+    playerLocations: framedState<PlayerCoordMap>;
+    playerDirections: framedState<PlayerDirectionMap>;
+    playerQueuedDirections: framedState<PlayerDirectionMap>;
 }
 
 const initialState: gameState = {
     playerLocations: {},
-    playerDirections: {}
+    playerDirections: {},
+    playerQueuedDirections: {}
 };
 
 export default (state: gameState = initialState, action: Action) => {
@@ -31,6 +35,8 @@ export default (state: gameState = initialState, action: Action) => {
             return { ...state, playerLocations: { ...state.playerLocations, [action.payload.frame]: action.payload.playerLocations } };
         case ActionTypes.RECORD_CURRENT_PLAYER_DIRECTIONS:
             return { ...state, playerDirections: { ...state.playerDirections, [action.payload.frame]: action.payload.playerDirections } };
+        case ActionTypes.RECORD_CURRENT_PLAYER_QUEUED_DIRECTIONS:
+            return { ...state, playerQueuedDirections: { ...state.playerDirections, [action.payload.frame]: action.payload.playerDirections } };
         default:
             return state;
     }
